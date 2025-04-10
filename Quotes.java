f35c6d4296f5e5ebc4850068dfd414541e746fbd
reyhan ovelek <reyhanovelek@gmail.com>
2025-04-10 22:55:24 +0200

    my third test

--- Quotes.java
+++ Quotes.java
@@ -1,7 +1,8 @@
 public class Quotes {
 public static void main (String [] args ) {
 System.out.println("Reyhan: \" My first awesome quote here ! \"");
-  System.out.println("Reyhan: \ " a second quote from the web!\"") ;
+  System.out.println("Reyhan: \" A second quote from the web! \"") ;
+  System.out.println("Reyhan: \" My third quote here ! \"");
 }
 }
 
